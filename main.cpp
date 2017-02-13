94a350ae1c6b66fbd18199788e2d52b455beb40a
Konstanty Misiak <kostu96@gmail.com>
2017-02-13 04:28:41 +0100

    Setup the window

--- main.cpp
+++ main.cpp
@@ -2,6 +2,24 @@
 
 int main()
 {
-	
-	return 0;
+	sf::RenderWindow window(sf::VideoMode(300, 350), "Tic Tac Toe", sf::Style::Close);
+
+	while (window.isOpen())
+	{
+		sf::Event e;
+		while (window.pollEvent(e))
+		{
+			switch (e.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			}
+		}
+		window.clear();
+
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
 }
