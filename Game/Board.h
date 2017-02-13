bdba78921dec89c941c378d806924f2773cbeffb
Konstanty Misiak <kostu96@gmail.com>
2017-02-13 05:41:53 +0100

    Move codebase into separate folder

---
+++ Game/Board.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+#include <SFML/System/NonCopyable.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include "Square.h"
+#include <array>
+
+class Board :
+	public sf::Drawable,
+	public sf::Transformable,
+	private sf::NonCopyable
+{
+private:
+	std::array<std::array<Square, 3>, 3> m_squares;
+
+	void draw(sf::RenderTarget & target, sf::RenderStates states) const;
+public:
+	Board();
+};
